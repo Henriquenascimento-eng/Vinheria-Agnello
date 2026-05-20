46f6e4ac3722c2eb00ab2e46a90d5c935cd52b87
henrique <riquesoares2007@gmail.com>
2026-05-19 21:23:33 -0300

    fix:adiciona-tela-dht11

--- codigo.c
+++ codigo.c
@@ -1,37 +1,100 @@
-const int pinLDR = A0;
-const int pinVerde = 8;
-const int pinAmarelo = 9;
+#include <LiquidCrystal.h>
+
+// LCD ligado nos pinos 2,3,4,5,6,7
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+// Sensores e atuadores
+const int pinLDR      = A0;
+const int pinVerde    = 8;
+const int pinAmarelo  = 9;
 const int pinVermelho = 10;
-const int pinBuzzer = 11;
+const int pinBuzzer   = 11;
+
+int estadoAnterior = -1;
 
 void setup() {
+
   pinMode(pinVerde, OUTPUT);
   pinMode(pinAmarelo, OUTPUT);
   pinMode(pinVermelho, OUTPUT);
   pinMode(pinBuzzer, OUTPUT);
+
   Serial.begin(9600);
+
+  // Inicia LCD
+  lcd.begin(16, 2);
+
+  // Mensagem inicial
+  lcd.setCursor(0, 0);
+  lcd.print("Eng. Software");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Monitor Luz");
+
+  delay(2000);
+
+  lcd.clear();
 }
 
 void loop() {
+
+  // Leitura do LDR
   int leitura = analogRead(pinLDR);
+
   Serial.println(leitura);
 
-  digitalWrite(pinVerde, LOW);
-  digitalWrite(pinAmarelo, LOW);
-  digitalWrite(pinVermelho, LOW);
-  digitalWrite(pinBuzzer, LOW);
+  // Estados
+  int estado;
 
-  if (leitura < 150) { 
-    digitalWrite(pinVerde, HIGH);
-  } 
-  else if (leitura >= 150 && leitura < 500) {
-    digitalWrite(pinAmarelo, HIGH);
-    digitalWrite(pinBuzzer, HIGH);
-    delay(3000);
-    digitalWrite(pinBuzzer, LOW);
-  } 
+  if (leitura > 850) {
+    estado = 0; // BOM
+  }
+  else if (leitura >= 400) {
+    estado = 1; // MEDIA
+  }
   else {
-    digitalWrite(pinVermelho, HIGH);
+    estado = 2; // RUIM
+  }
+
+  // LEDs
+  digitalWrite(pinVerde, estado == 0);
+  digitalWrite(pinAmarelo, estado == 1);
+  digitalWrite(pinVermelho, estado == 2);
+
+  // BUZZER
+  if (estado == 0) {
+
+    noTone(pinBuzzer);
+
+  }
+  else if (estado == 1) {
+
+    tone(pinBuzzer, 1000, 200);
+
+  }
+  else {
+
+    tone(pinBuzzer, 3000);
+
+  }
+
+  // LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Luz: ");
+  lcd.print(leitura);
+  lcd.print("    ");
+
+  lcd.setCursor(0, 1);
+
+  if (estado == 0) {
+
+    lcd.print("Status: BOM" );
+
+  }
+  else {
+
+    lcd.print("Alerta: Temp Alta");
+
   }
 
   delay(500);
